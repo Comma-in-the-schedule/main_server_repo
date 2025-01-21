761eb60afee5df3f9830eb1ee6273744ff672ecb
SHIMHUN <shimhun88@naver.com>
2025-01-22 02:16:29 +0900

    feat: 유저 엔티티 추가

---
+++ src/main/java/gdg/comma_in_the_schedule/domain/entity/User.java
@@ -0,0 +1,22 @@
+package gdg.comma_in_the_schedule.domain.entity;
+
+import gdg.comma_in_the_schedule.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String password;
+}
