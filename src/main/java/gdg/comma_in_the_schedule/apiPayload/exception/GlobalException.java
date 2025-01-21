5a6565ab27dd0522f11b1fbef246e5a2252b964a
SHIMHUN <shimhun88@naver.com>
2025-01-22 02:15:49 +0900

    chore: 회원가입 관련 핸들러 GlobalException에 등록

---
+++ src/main/java/gdg/comma_in_the_schedule/apiPayload/exception/GlobalException.java
@@ -0,0 +1,15 @@
+package gdg.comma_in_the_schedule.apiPayload.exception;
+
+import gdg.comma_in_the_schedule.apiPayload.ApiResponse;
+import gdg.comma_in_the_schedule.apiPayload.code.status.ErrorStatus;
+import gdg.comma_in_the_schedule.apiPayload.exception.handler.EmailAlreadyExistsHandler;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalException {
+    @ExceptionHandler(EmailAlreadyExistsHandler.class)
+    public ApiResponse<ErrorStatus> handleEmailAlreadyExistsException(EmailAlreadyExistsHandler handler) {
+        return ApiResponse.onFailure(handler.getErrorReason().getCode(), handler.getMessage(), ErrorStatus._INTERNAL_SERVER_ERROR);
+    }
+}
