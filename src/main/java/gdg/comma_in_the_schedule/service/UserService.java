6d276abb327f4753edcd6b906ad9848bae08ac08
SHIMHUN <shimhun88@naver.com>
2025-01-22 10:50:29 +0900

    feat: 회원가입시 비밀번호 암호화

--- src/main/java/gdg/comma_in_the_schedule/service/UserService.java
+++ src/main/java/gdg/comma_in_the_schedule/service/UserService.java
@@ -5,14 +5,19 @@ import gdg.comma_in_the_schedule.apiPayload.exception.handler.EmailAlreadyExists
 import gdg.comma_in_the_schedule.domain.entity.User;
 import gdg.comma_in_the_schedule.repository.UserRepository;
 import gdg.comma_in_the_schedule.web.dto.userdto.UserRequestDTO;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    UserService(UserRepository userRepository) {
+
+    UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public void registerUser(UserRequestDTO.RegisterUserRequestDTO registerUserRequestDTO) {
@@ -20,9 +25,11 @@ public class UserService {
             throw new EmailAlreadyExistsHandler(ErrorStatus._USER_EXISTS);
         }
 
+        String encodedPassword = passwordEncoder.encode(registerUserRequestDTO.getPassword());//비밀번호 암호화
+
         User user = User.builder()
                         .email(registerUserRequestDTO.getEmail())
-                        .password(registerUserRequestDTO.getPassword())
+                        .password(encodedPassword)
                         .build();
         userRepository.save(user);
     }
