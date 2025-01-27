aa4cf7dfcc0617258505169878561bd055feea9d
SHIMHUN <shimhun88@naver.com>
2025-01-27 18:22:18 +0900

    chore: Exception Handler 등록

--- src/main/java/gdg/comma_in_the_schedule/apiPayload/exception/GlobalException.java
+++ src/main/java/gdg/comma_in_the_schedule/apiPayload/exception/GlobalException.java
@@ -3,6 +3,8 @@ package gdg.comma_in_the_schedule.apiPayload.exception;
 import gdg.comma_in_the_schedule.apiPayload.ApiResponse;
 import gdg.comma_in_the_schedule.apiPayload.code.status.ErrorStatus;
 import gdg.comma_in_the_schedule.apiPayload.exception.handler.EmailAlreadyExistsHandler;
+import gdg.comma_in_the_schedule.apiPayload.exception.handler.EmailNotExistsHandler;
+import gdg.comma_in_the_schedule.apiPayload.exception.handler.PasswordNotMatchHandler;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +12,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalException {
     @ExceptionHandler(EmailAlreadyExistsHandler.class)
     public ApiResponse<ErrorStatus> handleEmailAlreadyExistsException(EmailAlreadyExistsHandler handler) {
+        return ApiResponse.onFailure(handler.getErrorReason().getCode(), handler.getMessage(), ErrorStatus._INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(EmailNotExistsHandler.class)
+    public ApiResponse<ErrorStatus> handleEmailNotExistsException(EmailNotExistsHandler handler) {
+        return ApiResponse.onFailure(handler.getErrorReason().getCode(), handler.getMessage(), ErrorStatus._INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(PasswordNotMatchHandler.class)
+    public ApiResponse<ErrorStatus> handlePasswordNotMatchException(PasswordNotMatchHandler handler) {
         return ApiResponse.onFailure(handler.getErrorReason().getCode(), handler.getMessage(), ErrorStatus._INTERNAL_SERVER_ERROR);
     }
 }
