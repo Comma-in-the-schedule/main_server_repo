5e6e10319c2bd4e4a4922a465e516368e58a5209
SHIMHUN <shimhun88@naver.com>
2025-02-02 22:57:27 +0900

    feat: 메일 토큰 엔티티 추가

---
+++ src/main/java/gdg/comma_in_the_schedule/domain/entity/EmailToken.java
@@ -0,0 +1,29 @@
+package gdg.comma_in_the_schedule.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String verificationCode; // UUID 기반 인증 코드
+
+    @Column
+    private LocalDateTime expiresAt; // 인증 코드 만료 시간
+}
