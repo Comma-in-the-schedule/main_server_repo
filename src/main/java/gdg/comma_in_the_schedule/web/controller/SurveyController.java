792d5eb2d4885ed4f3e638312f4b61c1405ddf67
hunseokim <gustj170@naver.com>
2025-02-09 00:48:23 +0900

    feat: 설문조사 api 추가

---
+++ src/main/java/gdg/comma_in_the_schedule/web/controller/SurveyController.java
@@ -0,0 +1,30 @@
+package gdg.comma_in_the_schedule.web.controller;
+
+import gdg.comma_in_the_schedule.apiPayload.ApiResponse;
+import gdg.comma_in_the_schedule.service.SurveyService;
+import gdg.comma_in_the_schedule.web.dto.userdto.SurveyRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/surveys")
+public class SurveyController {
+
+    private final SurveyService surveyService;
+
+    // POST: 설문 데이터 저장
+    @PostMapping("/save")
+    public ApiResponse<?> createSurvey(@RequestBody SurveyRequestDTO request) {
+        surveyService.createSurvey(request);
+        return ApiResponse.onSuccess(null);
+    }
+
+    // GET: 특정 userid로 설문 데이터 조회
+    @GetMapping("/user/{userid}")
+    public ApiResponse<List<?>> getSurveysByUserid(@PathVariable Long userid) {
+        return ApiResponse.onSuccess(surveyService.getSurveysByUserid(userid));
+    }
+}
