8c4596e71e6a7ac9031727803c8e1b6a79270e22
SHIMHUN <shimhun88@naver.com>
2025-02-18 16:47:51 +0900

    chore: 설문조사 요청 DTO 추가

--- src/main/java/gdg/comma_in_the_schedule/web/dto/userdto/UserRequestDTO.java
+++ src/main/java/gdg/comma_in_the_schedule/web/dto/userdto/UserRequestDTO.java
@@ -1,10 +1,13 @@
 package gdg.comma_in_the_schedule.web.dto.userdto;
 
+import gdg.comma_in_the_schedule.domain.entity.enums.SurveyCategory;
 import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class UserRequestDTO {
-
     private String email;
     private String password;
 
@@ -13,4 +16,21 @@ public class UserRequestDTO {
 //        private String email;
 //        private String password;
 //    }
+
+
+    @Getter
+    public static class SurveyUserRequestDTO{
+        private String email;
+        private String location;
+        private String nickname;
+        private List<Integer> category;
+
+        public List<SurveyCategory> getCategories() {
+            return category.stream()
+                    .map(SurveyCategory::fromCode) // 숫자를 ENUM으로 변환
+                    .collect(Collectors.toList());
+        }
+    }
+
+
 }
