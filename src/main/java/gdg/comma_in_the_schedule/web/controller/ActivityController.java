2bc396d213f0e6e7935407d8af50c6304a24c27a
SHIMHUN <shimhun88@naver.com>
2025-02-21 06:39:19 +0900

    chore: 컨트롤러 반환 타입 수정

--- src/main/java/gdg/comma_in_the_schedule/web/controller/ActivityController.java
+++ src/main/java/gdg/comma_in_the_schedule/web/controller/ActivityController.java
@@ -2,6 +2,7 @@ package gdg.comma_in_the_schedule.web.controller;
 
 import gdg.comma_in_the_schedule.apiPayload.ApiResponse;
 import gdg.comma_in_the_schedule.service.ActivityService;
+import gdg.comma_in_the_schedule.web.dto.activitydto.ActivityResponseDTO;
 import gdg.comma_in_the_schedule.web.dto.userdto.UserRequestDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -19,7 +22,7 @@ public class ActivityController {
 
     @PostMapping("/recommendation")
     public ApiResponse<?> recommendAll(@RequestBody UserRequestDTO userRequestDTO){
-        activityService.recommendAll(userRequestDTO);
-        return null;
+        List<List<ActivityResponseDTO>> activityResponseDTO = activityService.recommendAll(userRequestDTO);
+        return ApiResponse.onSuccess(activityResponseDTO);
     }
 }
